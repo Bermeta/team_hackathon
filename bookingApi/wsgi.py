84678d73feacd855d60779b9d66de90e453907c1
bermeta <bermetzarlyk@gmail.com>
2023-01-20 20:55:07 +0600

    initial commit

--- bookingApi/wsgi.py
+++ bookingApi/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projectApi project.
+WSGI config for bookingApi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectApi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookingApi.settings')
 
 application = get_wsgi_application()
